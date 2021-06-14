7e1ddde8bfde11fb68f55dbc265db8d9f547fe84
0429oonishi <oonishireon110@gmail.com>
2021-06-14 20:10:08 +0900

    RepeatVC修正

--- Alarm-Sample/Controllers/AlarmRepeatViewController.swift
+++ Alarm-Sample/Controllers/AlarmRepeatViewController.swift
@@ -7,23 +7,81 @@
 
 import UIKit
 
-class AlarmRepeatViewController: UIViewController {
+protocol AlarmRepeatVCDelegate: AnyObject {
+    func alarmRepeatVC(addRepeat: AlarmRepeatViewController, weeks: [String])
+}
 
+final class AlarmRepeatViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    weak var delegate: AlarmRepeatVCDelegate?
+    private var weeks = DateFormatter().weekdaySymbols!
+    var selectedDays = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        delegate?.alarmRepeatVC(addRepeat: self, weeks: sort(selectedDays))
+        
     }
-    */
-
+    
+    private func sort(_ selectDays: [String]) ->  [String] {
+        let weeks = DateFormatter().weekdaySymbols!
+        var dayDictionary = [String: Int]()
+        for i in 0..<weeks.count {
+            dayDictionary[weeks[i]] = i
+        }
+        var daysOfWeek = selectDays
+        daysOfWeek.sort { (dayDictionary[$0] ?? 7) < (dayDictionary[$1] ?? 7) }
+        return daysOfWeek
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsMultipleSelection = true
+        tableView.tableFooterView = UIView()
+    }
+    
 }
+
+extension AlarmRepeatViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+        selectedDays.append(weeks[indexPath.row])
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .none
+        selectedDays = selectedDays.filter { $0 != weeks[indexPath.row] }
+    }
+    
+}
+
+extension AlarmRepeatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weeks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AlarmRepeatVCCellID", for: indexPath)
+        cell.textLabel!.text = "Every" + weeks[indexPath.row]
+        cell.selectionStyle = .none
+        selectedDays.forEach { cell.accessoryType = (weeks[indexPath.row] == $0) ? .checkmark : .none }
+        return cell
+    }
+    
+}
+

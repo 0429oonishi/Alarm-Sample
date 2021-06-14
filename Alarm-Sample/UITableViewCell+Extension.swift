c6bb4b63258be6fedf53834946262a958785beff
0429oonishi <oonishireon110@gmail.com>
2021-06-14 20:09:52 +0900

    UITableViewCell+Extension実装

--- Alarm-Sample/UITableViewCell+Extension.swift
+++ Alarm-Sample/UITableViewCell+Extension.swift
@@ -5,4 +5,11 @@
 //  Created by 大西玲音 on 2021/06/14.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell {
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+}

c15c0216c8e08c350b8c8c4c15ea26cc0cb4fa2e
0429oonishi <oonishireon110@gmail.com>
2021-06-14 12:38:24 +0900

    NotificationName+Extension追加

---
+++ Alarm-Sample/NotificationName+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationName+Extension.swift
+//  Alarm-Sample
+//
+//  Created by 大西玲音 on 2021/06/14.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static var notificationIdentifier: Self {
+        return Notification.Name("NotificationIdentifier")
+    }
+    
+}
+
